9309105400a5fd1f1f83510e07d368678348453b
Wendryl <wendryl10000@gmail.com>
2021-04-18 12:48:46 -0300

    add product crud service

---
+++ src/app/Services/product.service.ts
@@ -0,0 +1,25 @@
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {IProduct} from '../Models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  api = environment.apiUrl;
+  token = localStorage.getItem('authToken')
+
+  constructor(private _httpClient: HttpClient) { }
+
+  list(): Observable<Array<IProduct>> {
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+    })
+
+    return this._httpClient.get<Array<IProduct>>(`${this.api}products`, {headers});
+  }
+}
