30c2dfd4ca64b04a29420a2f1232d664346ef4ca
Wendryl <wendryl10000@gmail.com>
2021-05-09 13:35:18 -0300

    correction on server send file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
 
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 
